d36a1f916b7234b8fe0a7be749325aca4baad376
liqiang <liqiang@makeblock.com>
2023-07-26 15:42:26 +0800

    feat:nfc 标签分发和 nfc 前台分发

--- app/src/main/java/cn/blogss/nfc/MainActivity.kt
+++ app/src/main/java/cn/blogss/nfc/MainActivity.kt
@@ -1,15 +1,12 @@
 package cn.blogss.nfc
 
-import android.app.PendingIntent
 import android.content.Intent
-import android.content.IntentFilter
 import android.nfc.NfcAdapter
 import android.nfc.Tag
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -20,8 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import cn.blogss.nfc.ui.theme.AndroidnfcdemoTheme
 
-class MainActivity : ComponentActivity() {
-    private var nfcAdapter: NfcAdapter? = null
+class MainActivity : NfcBaseActivity() {
 
     companion object {
         private const val TAG = "MainActivity"
@@ -40,31 +36,7 @@ class MainActivity : ComponentActivity() {
         initData()
     }
 
-    override fun onResume() {
-        super.onResume()
-        val pendingIntent = PendingIntent.getActivity(
-            this,
-            0,
-            Intent(this, javaClass),
-            /**
-             * 注意 flags 的值设置为 FLAG_IMMUTABLE 会导致 intent 中的数据为空
-             */
-            PendingIntent.FLAG_MUTABLE
-        )
-        // 启用 nfc 前台调度, 该 activity 处理 nfc 标签的优先级会优先于在 <intent-filter> 中配置的 Activity
-        nfcAdapter?.enableForegroundDispatch(this,
-            pendingIntent, null, null)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        // 关闭 nfc 前台调度
-        nfcAdapter?.disableForegroundDispatch(this)
-    }
-
-
     private fun initData() {
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
         if(nfcAdapter == null) {
             Toast.makeText(this, "该设备不支持 nfc", Toast.LENGTH_LONG).show();
             finish()
@@ -75,6 +47,7 @@ class MainActivity : ComponentActivity() {
             startActivity(Intent(Settings.ACTION_NFC_SETTINGS))
             Toast.makeText(this, "设备未开启 nfc", Toast.LENGTH_LONG).show()
         }
+        readNfcTagData(intent)
     }
 
     /**
@@ -82,24 +55,29 @@ class MainActivity : ComponentActivity() {
      */
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
-        if(NfcAdapter.ACTION_NDEF_DISCOVERED == intent?.action ||
-                NfcAdapter.ACTION_TECH_DISCOVERED == intent?.action ||
-                NfcAdapter.ACTION_TAG_DISCOVERED == intent?.action) {
+        readNfcTagData(intent)
+
+    }
+
+    private fun readNfcTagData(intent: Intent?) {
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent?.action ||
+            NfcAdapter.ACTION_TECH_DISCOVERED == intent?.action ||
+            NfcAdapter.ACTION_TAG_DISCOVERED == intent?.action
+        ) {
             val tag: Tag? = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
             val id = bytesToHexString(tag!!.id)
             Log.i(TAG, "nfc id: $id")
 
             // 获取tag 中的数据信息
             val tagTechList = tag.techList
-            if(tagTechList != null && tagTechList.isNotEmpty()) {
+            if (tagTechList != null && tagTechList.isNotEmpty()) {
                 val sb = StringBuilder()
-                for(i in tagTechList) {
+                for (i in tagTechList) {
                     sb.append("*").append(i).append("*").append("\n")
                 }
                 Log.i(TAG, "tagTechList: $sb}")
             }
         }
-
     }
 
     private fun bytesToHexString(bytes: ByteArray): String {
@@ -108,10 +86,6 @@ class MainActivity : ComponentActivity() {
             sb.append(String.format("%02x", b))
         }
         return sb.toString()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
     }
 }
 
---
+++ app/src/main/java/cn/blogss/nfc/NfcBaseActivity.kt
@@ -0,0 +1,60 @@
+package cn.blogss.nfc
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NfcAdapter
+import android.nfc.tech.NdefFormatable
+import android.nfc.tech.NfcV
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+
+open class NfcBaseActivity: ComponentActivity() {
+    protected var nfcAdapter: NfcAdapter? = null
+    private var filters: Array<IntentFilter>? = null
+    private var techLists: Array<Array<String>>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        filters = arrayOf(IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED))
+        techLists = arrayOf(arrayOf("android.nfc.tech.NfcV",
+            "android.nfc.tech.NdefFormatable"))
+    }
+
+    /**
+     * 在 onResume 方法中启用 nfc 前台分发
+     * Foreground dispatch can only be enabled when your activity is resumed.
+     */
+    override fun onResume() {
+        super.onResume()
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            Intent(this, javaClass),
+            /**
+             * 注意 flags 的值设置为 FLAG_IMMUTABLE 会导致 intent 中的数据为空
+             */
+            PendingIntent.FLAG_MUTABLE
+        )
+        /**
+         * 启用 nfc 前台分发, 该 activity 位于前台时，nfc 标签会直接交给该 Activity 处理，而不是 <intent-filter> 中配置的 Activity
+         * 若 filters 和 techLists 参数同时为空，会导致该 activity 接收 action 为 ACTION_TAG_DISCOVERED
+         * 的 intent
+         */
+        nfcAdapter?.enableForegroundDispatch(this,
+            pendingIntent, filters, techLists)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // 关闭 nfc 前台调度
+        nfcAdapter?.disableForegroundDispatch(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+}
